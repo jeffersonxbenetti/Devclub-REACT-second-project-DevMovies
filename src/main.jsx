a87a673d8b33df63be0ce53ad6f787af19fa6d70
jeffersonxbenetti <jefferson.benetti@hotmail.com>
2024-10-17 17:59:46 -0300

    Instalando styled-components e criando o estilos globais para o projeto.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import Router from './routes/routes.jsx'
+import GlobalStyles from './styles/globalStyles.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    <GlobalStyles />
   </StrictMode>,
 )
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+        text-decoration: none;
+        font-family: "Montserrat", sans-serif;
+    }
+
+    body {
+        background-color: #000000;
+    }
+`
+
+export default GlobalStyles
