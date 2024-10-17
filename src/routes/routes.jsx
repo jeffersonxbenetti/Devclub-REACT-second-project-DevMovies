312434edbd41749f644d84103455eee8ea5d3dbd
jeffersonxbenetti <jefferson.benetti@hotmail.com>
2024-10-17 15:56:38 -0300

    Adicionando rotas ao projeto.

---
+++ src/routes/routes.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from '../containers/Home'
+import Movies from '../containers/Movies'
+import Series from '../containers/Series'
+
+function Router() {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+        </Routes>
+    )
+}
+
+export default Router
