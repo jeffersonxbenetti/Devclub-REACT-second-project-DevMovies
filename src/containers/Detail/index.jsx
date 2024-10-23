1690a9952c8da2a1cc8819a894939ecd2070329c
jeffersonxbenetti <jefferson.benetti@hotmail.com>
2024-10-23 14:36:29 -0300

    Organizando os dados da tela de detalhe.

--- src/containers/Detail/index.jsx
+++ src/containers/Detail/index.jsx
@@ -1,10 +1,45 @@
-function Detail(){
-    
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { getMovieCredits, getMovieDetailsById, getMovieSimilar, getMovieVideos } from '../../services/getData.js'
+import { Container } from './styles.js'
+
+function Detail() {
+    const [movieDetails, setMovieDetails] = useState()
+    const [movieVideos, setMovieVideos] = useState()
+    const [movieCredits, setMovieCredits] = useState()
+    const [movieSimilar, setMovieSimilar] = useState()
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        async function getAllData() {
+            Promise.all([
+                getMovieDetailsById(id),
+                getMovieVideos(id),
+                getMovieCredits(id),
+                getMovieSimilar(id)
+            ])
+                .then(([details, videos, credits, similar]) => {
+
+                    console.log({ details, videos, credits, similar })
+
+                    setMovieDetails(details)
+                    setMovieVideos(videos)
+                    setMovieCredits(credits)
+                    setMovieSimilar(similar)
+                })
+                .catch((error) => console.error(error))
+        }
+
+        getAllData()
+    }, [])
+
     return (
-        <div>
+        <Container>
             <h1>DETALHE</h1>
             <p>Esse é a DETALHE</p>
-        </div>
+        </Container>
     )
 }
 
--- src/services/getData.js
+++ src/services/getData.js
@@ -3,36 +3,53 @@ import api from './api.js'
 export async function getMovie() {
     const { data: { results } } = await api.get('/movie/popular')
 
-    return (results[0])
+    return results[0]
 }
 
 export async function getTopMovies() {
     const { data: { results } } = await api.get("/movie/top_rated")
 
-    return (results)
+    return results
 }
 
 export async function getTopSeries() {
     const { data: { results } } = await api.get("/tv/top_rated")
 
-    return (results)
+    return results
 }
 
 export async function getPopularSeries() {
     const { data: { results } } = await api.get("/tv/popular")
 
-    return (results)
+    return results
 }
 
 export async function getTopPeoples() {
     const { data: { results } } = await api.get("/person/popular")
 
-    return (results)
+    return results
 }
 
 export async function getMovieVideos(movieId) {
-    console.log(movieId)
     const { data: { results } } = await api.get(`/movie/${movieId}/videos`)
 
-    return (results[0])
+    return results
+}
+
+export async function getMovieCredits(movieId) {
+    const { data } = await api.get(`/movie/${movieId}/credits`)
+
+    return data
+}
+
+export async function getMovieSimilar(movieId) {
+    const { data: { results } } = await api.get(`/movie/${movieId}/similar`)
+
+    return results[0]
+}
+
+export async function getMovieDetailsById(movieId) {
+    const { data } = await api.get(`/movie/${movieId}`)
+
+    return data
 }
